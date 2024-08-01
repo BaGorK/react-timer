f058268225a5d34e8a4ac79d12028270a8cc1435
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-08-01 14:19:44 +0300

    feat: using forwardRef with Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
+import { useRef } from 'react';
 import Button from './components/UI/Button';
 import Container from './components/UI/Container';
 import Input from './components/UI/Input';
 
 function App() {
+  const inputRef = useRef<HTMLInputElement>(null);
   return (
     <main className='flex flex-col gap-3'>
       <Input id='name' label='Your name' type='text' />
-      <Input id='age' label='Your age' type='number' />
+      <Input id='age' label='Your age' type='number' ref={inputRef} />
 
       <p>
         <Button>button</Button>
@@ -15,9 +17,7 @@ function App() {
         <Button href='something.com'>button</Button>
       </p>
       <p>
-        <Container as='button' >
-          click me
-        </Container>
+        <Container as='button'>click me</Container>
       </p>
     </main>
   );
